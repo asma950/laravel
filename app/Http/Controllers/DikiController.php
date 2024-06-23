7bd7cc49bf477a065d2dfa0b877def7bf9783430
asma950 <watia2721@gmail.com>
2024-06-23 15:03:05 +0800

    first and completed commit

---
+++ app/Http/Controllers/DikiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
+
+
+class DikiController extends Controller
+{
+    public function data()
+    {
+        $parameter = [
+            'nama' => 'Diki Alfarabi Hadi',
+            'pekerjaan' => 'Programmer',
+        ];
+        $enkripsi = Crypt::encrypt($parameter);
+        echo "<a href='/data/" . $enkripsi . "'>Klik</a>";
+    }
+
+    public function data_proses($data)
+    {
+        $data = Crypt::decrypt($data);
+
+        echo "Nama : " . $data['nama'];
+        echo "<br/>";
+        echo "Pekerjaan : " . $data['pekerjaan'];
+    }
+    public function hash(){
+        $hash_password_saya = Hash::make('halo123');
+        echo $hash_password_saya;
+}
+}
